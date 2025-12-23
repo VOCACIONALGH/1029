eb85e6ef357f25a3d9ab4d344075e18f2c18c394
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 15:29:18 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,28 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = "scanner-cache-v1";
+
+const FILES = [
+  "index.html",
+  "manifest.json",
+
+  "css/estrutura.css",
+  "css/interface.css",
+
+  "js/estrutura.js",
+  "js/interface.js",
+  "js/geometria.js",
+  "js/exportacao.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
