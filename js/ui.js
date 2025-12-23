4a04f6202eb3c78fae976fac3374a0e661f06088
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 10:37:31 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,16 @@
 export function initUI(button, startCallback) {
     button.addEventListener("click", () => {
-        document.getElementById("scannerContainer").style.display = "flex";
+        const container = document.getElementById("scannerContainer");
+        container.style.display = "flex";
         button.style.display = "none";
+
+        const video = document.getElementById("cameraFeed");
+        const black = document.getElementById("blackScreen");
+
+        // Ajuste explícito para duas partes iguais
+        video.style.height = "50%";
+        black.style.height = "50%";
+
         startCallback();
     });
 }
