8064376c1ede883d750fbbb9544236e5e5e11989
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 15:36:31 -0300

    Update estrutura.js

--- js/estrutura.js
+++ js/estrutura.js
@@ -1,4 +1,8 @@
 export const elements = {
   video: document.getElementById("camera"),
-  scanBtn: document.getElementById("scanBtn")
+  scanBtn: document.getElementById("scanBtn"),
+  canvas: document.getElementById("frame"),
+  pixelInfo: document.getElementById("pixelInfo")
 };
+
+export const ctx = elements.canvas.getContext("2d");
