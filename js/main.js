4e51ab5252f56ffabde8d6eb460d0cdedcbab9b0
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 10:52:58 -0300

    Delete js/main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// main.js - entrada
-import { setupUI } from "./ui.js";
-import { registerServiceWorker } from "./sw-register.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupUI();
-  registerServiceWorker();
-});
