d028d1e388635037f3335115d412e2a91613c7da
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 12:14:27 -0300

    Update interface.js

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,6 @@
 const scanButton = document.getElementById('scanButton');
 const video = document.getElementById('camera');
+const counter = document.getElementById('redPixelCounter');
 
 scanButton.addEventListener('click', async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
@@ -9,3 +10,12 @@ scanButton.addEventListener('click', async () => {
 
   video.srcObject = stream;
 });
+
+function atualizarContador() {
+  if (typeof window.redPixelCount === 'number') {
+    counter.textContent = `Pixels vermelhos: ${window.redPixelCount}`;
+  }
+  requestAnimationFrame(atualizarContador);
+}
+
+requestAnimationFrame(atualizarContador);
