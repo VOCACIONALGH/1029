4cc349d7632d319b32243e6456d855a56fd1f5f5
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 15:45:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,41 @@
 const CACHE_NAME = "scanner-cache-v1";
-
-const FILES = [
-  "index.html",
-  "manifest.json",
-
-  "css/estrutura.css",
-  "css/interface.css",
-
-  "js/estrutura.js",
-  "js/interface.js",
-  "js/geometria.js",
-  "js/exportacao.js"
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./layout.css",
+    "./main.js",
+    "./export.js",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
