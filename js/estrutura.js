7ad82f1ffd14e9b8087898a2add1c96918fe20e3
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 15:44:02 -0300

    Delete js directory

--- js/estrutura.js
+++
@@ -1,8 +0,0 @@
-export const elements = {
-  video: document.getElementById("camera"),
-  scanBtn: document.getElementById("scanBtn"),
-  canvas: document.getElementById("frame"),
-  pixelInfo: document.getElementById("pixelInfo")
-};
-
-export const ctx = elements.canvas.getContext("2d");
