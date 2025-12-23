86bb811767604f155ff82b7dc53c02e6637ff5d5
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 10:47:39 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-import { startScanner } from "./scanner.js";
-import { initUI } from "./ui.js";
+// main.js - entrada
+import { setupUI } from "./ui.js";
+import { registerServiceWorker } from "./sw-register.js";
 
-initUI(document.getElementById("btnStart"), startScanner);
+document.addEventListener("DOMContentLoaded", () => {
+  setupUI();
+  registerServiceWorker();
+});
