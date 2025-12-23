57604e827cdfa912dce8793ec57dfb79e874903d
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-23 12:02:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", event => {
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
