77bb739f10801cd67bde9c30697c74abe994dcd4
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-25 21:57:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const scanBtn = document.getElementById("scanBtn");
 const video = document.getElementById("camera");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 scanBtn.addEventListener("click", async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -10,4 +12,32 @@ scanBtn.addEventListener("click", async () => {
     });
 
     video.srcObject = stream;
+
+    video.onloadedmetadata = () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        processFrame();
+    };
 });
+
+function processFrame() {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = frame.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+
+        if (r < 20 && g < 20 && b < 20) {
+            data[i] = 255;     // R (laranja)
+            data[i + 1] = 165; // G
+            data[i + 2] = 0;   // B
+        }
+    }
+
+    ctx.putImageData(frame, 0, 0);
+    requestAnimationFrame(processFrame);
+}
